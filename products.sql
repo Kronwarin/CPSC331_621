826320a0e0dd2343db45f851c875ebc2ffbc8bbd
Kronwarin <42400942+Kronwarin@users.noreply.github.com>
2019-09-19 03:49:32 +0700

    Add files via upload

---
+++ products.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 18, 2019 at 09:36 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `products`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_products`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_products` (
+  `productid` int(4) NOT NULL,
+  `productname` varchar(50) NOT NULL,
+  `supplierid` int(50) NOT NULL,
+  `categoryid` int(50) NOT NULL,
+  `quantityperunit` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
