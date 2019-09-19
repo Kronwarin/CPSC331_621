b2d30b432fffad020f188ab38ef1c226cf4a8030
Kronwarin <42400942+Kronwarin@users.noreply.github.com>
2019-09-19 10:38:49 +0700

    Delete pro_s7drop.php

--- pro_s7drop.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("pro_s1connect.php");
-if (!$result=mysql_query("drop database $db",$connect))
-  echo "Drop database : failed";
-else
-  echo "Drop database : succeeded";
-mysql_close($connect);
-?>
-<input type='button' onclick="location.href='pro_s0index.php'" value='back'>
